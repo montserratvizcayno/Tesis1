60efce87756ceaa8dc69397dbd57e16a61b18c07
Juan Carlos Martínez-Ovando <jc.martinez.ovando@gmail.com>
2016-11-28 11:38:45 -0600

    update jcmo

---
+++ Gibbs Sampler/dist_inciales.R
@@ -0,0 +1,48 @@
+## esta funci?n calcula las distribuciones previas, toma como valor por default que sean dos componentes
+##en la funci?n se pueden tomar 1 o mas variables, en el ejemplo solo ocupo 1
+dist_inciales <- function(datos, nom.var = names(datos), componente = 2){
+  
+  
+  est.init <- estadisticos_iniciales(datos) %>%
+    data.frame()
+  
+  k <- componente
+  
+  ## hiperparámetros iniciales que en este caso toman valores de 1 y pi= 1 / #de componentes
+  nj <- rep(1, k)
+  vj <- rep(1, k)
+  sj <- rep(1, k)
+  pi <- rep(1/k, k)
+  
+  ## calcular gamma inversa (cuando se tiene una variable)
+  aux.sigma0 <- data.frame(v = vj,
+                           s = sj) %>%
+    rowwise() %>% 
+    mutate(sim = rinvgamma(1, v/2, s/2)) %>%
+    data.frame
+  
+  sigma0 <- aux.sigma0$sim
+  
+  ## calcular normal (cuando se tiene una variable)
+  aux.mu0 <- data.frame(x_barra = rep(est.init[which(est.init$variable == nom.var),2], k),
+                        sigma = sigma0,
+                        n = nj) %>%
+    rowwise() %>%
+    mutate(sim = rnorm(1, x_barra, sigma/n)) %>%
+    data.frame()
+  
+  mu0 <- aux.mu0$sim
+  
+  ## parámetros de las distribuciones inciales
+  param.init <- data.frame(componente = 1:k,
+                           mu_inicial = mu0,
+                           sigma_inicial = sigma0,
+                           pi = pi,
+                           nj = nj,
+                           vj = vj,
+                           sj = sj)
+  
+  return(param.init)
+  
+  
+}
---
+++ Gibbs Sampler/rscript_jcmo.R
@@ -0,0 +1,40 @@
+
+#install.packages("repmis")
+#install.packages("tidyverse")
+#install.packages("MCMCpack")
+#install.packages("LCA")
+
+require("repmis")
+require("tidyverse") 
+require("MCMCpack")  
+require("LCA") 
+
+rm(list=ls())
+setwd("C:/Users/jmartineov/Documents/GitHub/Tesis1/Gibbs Sampler/")
+
+# source("https://raw.githubusercontent.com/montserratvizcayno/Gibbs Sampler/funciones.R")
+source("C:/Users/jmartineov/Documents/GitHub/Tesis1/Gibbs Sampler/funciones.R")
+#source("C:/Users/jmartineov/Documents/GitHub/Tesis1/Gibbs Sampler/simulaciones.R")
+#source("C:/Users/jmartineov/Documents/GitHub/Tesis1/Gibbs Sampler/dist_inciales.R")
+#source("C:/Users/jmartineov/Documents/GitHub/Tesis1/Gibbs Sampler/estadisticos_iniciales.R")
+
+# Data
+data.clientes <- read.csv('Clientes_17092105.csv',
+                          stringsAsFactors = FALSE)
+head(data.clientes)
+glimpse(data.clientes)
+
+
+hiper.param <- estadisticos_iniciales(data.clientes)
+
+param.iniciales <- dist_inciales(data.clientes, nom.var = "Monto_prom", componente = 2)
+
+
+prueba_univar <- simulaciones(datos = data.clientes, nom.var = 'Creditos', componente = 2,
+                              folio = 'Cliente', iteraciones = 10)
+
+prueba_univar %>%
+  dplyr::select(sim, componente, pi) %>% 
+  ggplot(aes(x = sim, y = pi, group = as.factor(componente), colour = as.factor(componente))) +
+  geom_point() +
+  geom_line()
